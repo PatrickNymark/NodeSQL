3b8e27e9c668dc2a62f405652ee460436699831c
Patrick Nymark <pn@ctdenmark.dk>
2019-04-08 22:16:40 +0200

    refactored sequelize config to database config

---
+++ config/database.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const keys = require('./keys');
+
+// models
+const ArtistModel = require('../models/artist');
+const SongModel = require('../models/song');
+const AlbumModel = require('../models/album');
+
+const sequelize = new Sequelize(keys.database, keys.username, keys.password, {
+  host: keys.host,
+  dialect: keys.dialect,
+  define: {
+    timestamps: false
+  }
+})
+
+// Create tables if not exists
+sequelize.authenticate().then(() => console.log('Database connected')).catch(err => console.error(err))
+
+const Artist = ArtistModel(sequelize, Sequelize);
+const Song = SongModel(sequelize, Sequelize);
+const Album = AlbumModel(sequelize, Sequelize);
+
+module.exports = {
+  Artist,
+  Song,
+  Album
+}
