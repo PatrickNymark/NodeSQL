b6f4aba2971772f1ba9cac7267024e940b5973e6
Patrick Nymark <pn@ctdenmark.dk>
2019-04-12 10:42:37 +0200

    added get song by id route

--- routes/songs.js
+++ routes/songs.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // models
 const { Song } = require('../config/database');
 
+/**
+ * GET all songs
+ */
 router.get('/', (req, res) => {
   Song.findAll().then(songs => {
     if (!songs) {
@@ -14,11 +17,32 @@ router.get('/', (req, res) => {
   })
 })
 
+/**
+ * POST add song
+ */
 router.post('/', (req, res) => {
   const song = Object.assign(req.body);
 
   Song.create(song).then(song => res.json(song)).catch(err => res.status(500).json(err))
 })
 
+/**
+ * GET song by id
+ */
+router.get('/find/:song_id', (req, res) => {
+  const { song_id } = req.params;
+
+  Song.findAll({
+    where: {
+      song_id: song_id
+    }
+  }).then(song => {
+    if (!song) {
+      return res.status(404).json({ msg: 'Song not found' })
+    }
+
+    res.json(song)
+  }).catch(err => res.status(500).json(err))
+})
 
 module.exports = router;
