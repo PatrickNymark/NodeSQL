bd1741f02fc027ef4f50d4733a5547ffe00b80bd
Patrick Nymark <pn@ctdenmark.dk>
2019-04-12 10:42:55 +0200

    added get artist by id route

--- routes/artists.js
+++ routes/artists.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { Artist } = require('../config/database');
 
 /**
- * GET find all artists
+ * GET all artists
  */
 router.get('/', (req, res) => {
   Artist.findAll().then(artists => {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     }
 
     res.json(artists);
-  })
+  }).catch(err => res.status(500).json(err))
 })
 
 /**
@@ -24,6 +24,27 @@ router.post('/', (req, res) => {
   const artist = Object.assign(req.body);
 
   Artist.create(artist).then(artist => res.json(artist)).catch(err => res.status(500).json(err))
+});
+
+/**
+ * GET artist by id
+ */
+router.get('/find/:artist_id', (req, res) => {
+  const { artist_id } = req.params;
+
+  Artist.findAll({
+    where: {
+      artist_id: artist_id
+    }
+  }).then(artist => {
+    if (!artist) {
+      return res.status(404).json({ msg: 'Artist not found' })
+    }
+
+    res.json(artist)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 })
 
 module.exports = router;
